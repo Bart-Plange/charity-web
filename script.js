50b73ea3e804400cf6fa25fc4715ffd4a0beee77
Bart-Plange <87731837+Bart-Plange@users.noreply.github.com>
2023-01-13 01:54:35 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const btn = document.getElementById('navbtn')
+const btnmenu = document.getElementById('menu')
+
+btn.addEventListener('click', () => {
+    btn.classList.toggle('open')
+    btnmenu.classList.toggle('flex')
+    btnmenu.classList.toggle('hidden')
+})
