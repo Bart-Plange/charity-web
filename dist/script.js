86244d4288347477f3cb80016430eba9b33de574
Bart-Plange <87731837+Bart-Plange@users.noreply.github.com>
2023-01-13 01:52:48 +0000

    Delete dist directory

--- dist/script.js
+++
@@ -1,8 +0,0 @@
-const btn = document.getElementById('navbtn')
-const btnmenu = document.getElementById('menu')
-
-btn.addEventListener('click', () => {
-    btn.classList.toggle('open')
-    btnmenu.classList.toggle('flex')
-    btnmenu.classList.toggle('hidden')
-})
